b643a58aa8008133f8e49963a5a07dfae856cf1a
dsz <1019217919@qq.com>
2026-02-20 13:34:31 +0800

    chore: 主页轮播图高度调整为1024px

--- web/src/templates/010/homeTemplate.jsx
+++ web/src/templates/010/homeTemplate.jsx
@@ -25,7 +25,7 @@ export default function HomeTemplate({
 
             {
                 bannerData?.length > 0 && (
-                    <div className="w-full h-[200px] md:h-[calc((100vh-80px)_/_2)] lg:h-[500px]">
+                    <div className="w-full h-[200px] md:h-[calc((100vh-80px)_/_2)] lg:h-[1040px]">
                         <Carousel bannerData={bannerData} heroText={heroText}/>
                     </div>
                 )
