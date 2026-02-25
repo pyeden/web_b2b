ee570b043e530dad194ac762a9536d2ecaac09f9
dsz <1019217919@qq.com>
2026-02-25 23:00:29 +0800

    chore: footer样式

--- web/src/components/index/sections/template10/footer.jsx
+++ web/src/components/index/sections/template10/footer.jsx
@@ -10,14 +10,14 @@ export default function Footer({sectionData}) {
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
                         <div className="text-center md:text-left">
-                            <h3 className="text-lg font-semibold text-gray-900 mb-2 pb-2 ">
+                            <h3 className="text-lg font-semibold text-white mb-2 pb-2 ">
                                 {lang.QuickLinks}
                             </h3>
                             <ul className="space-y-2">
                                 {sectionData.navData.map((item) => (
                                     <li key={item.name}>
                                         <Link href={item.href}
-                                              className="text-gray-600 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">
+                                              className="text-white\/50 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">
                                             <svg className="w-4 h-4 text-mainColorNormal mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                                 <path fill="currentColor" d="M10.5 16.3q-.2 0-.35-.137T10 15.8V8.2q0-.225.15-.362t.35-.138q.05 0 .35.15l3.625 3.625q.125.125.175.25t.05.275t-.05.275t-.175.25L10.85 16.15q-.075.075-.162.113t-.188.037"/>
                                             </svg>
@@ -29,14 +29,14 @@ export default function Footer({sectionData}) {
                         </div>
                         {/* 产品分类 */}
                         <div className="text-center md:text-left">
-                            <h3 className="text-lg font-semibold text-gray-900 mb-2 pb-2">
+                            <h3 className="text-lg font-semibold text-white mb-2 pb-2">
                                 {lang.ProductCategories}
                             </h3>
                             <ul className="space-y-2">
                                 {sectionData.categoryData.map((item) => (
                                     <li key={item.id}>
                                         <Link href={'/product/category/' + item.id}
-                                              className="text-gray-600 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">
+                                              className="text-white\/50 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">
                                             <svg className="w-4 h-4 text-mainColorNormal mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                                 <path fill="currentColor" d="M10.5 16.3q-.2 0-.35-.137T10 15.8V8.2q0-.225.15-.362t.35-.138q.05 0 .35.15l3.625 3.625q.125.125.175.25t.05.275t-.05.275t-.175.25L10.85 16.15q-.075.075-.162.113t-.188.037"/>
                                             </svg>
@@ -47,7 +47,7 @@ export default function Footer({sectionData}) {
                             </ul>
                         </div>
                         <div className="text-center md:text-left">
-                            <h3 className="text-lg font-semibold text-gray-900 mb-2 pb-2">
+                            <h3 className="text-lg font-semibold text-white mb-2 pb-2">
                                 {lang.ContactUs}
                             </h3>
                             <ul className="space-y-3">
@@ -59,7 +59,7 @@ export default function Footer({sectionData}) {
                                                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                                         </svg>
                                     </div>
-                                    <span className="text-gray-600">{sectionData.contactData.global_email}</span>
+                                    <span className="text-white\/50">{sectionData.contactData.global_email}</span>
                                 </li>
                                 <li className="flex items-start justify-center md:justify-start">
                                     <div className="text-mainColorNormal mr-2 mt-1">
@@ -69,7 +69,7 @@ export default function Footer({sectionData}) {
                                                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                                         </svg>
                                     </div>
-                                    <span className="text-gray-600">{sectionData.contactData.global_phone}</span>
+                                    <span className="text-white\/50">{sectionData.contactData.global_phone}</span>
                                 </li>
                                 <li className="flex items-start justify-center md:justify-start">
                                     <div className="text-mainColorNormal mr-2 mt-1">
@@ -81,7 +81,7 @@ export default function Footer({sectionData}) {
                                                   d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                                         </svg>
                                     </div>
-                                    <span className="text-gray-600">{sectionData.contactData.global_address}</span>
+                                    <span className="text-white\/50">{sectionData.contactData.global_address}</span>
                                 </li>
                             </ul>
                         </div>
@@ -102,7 +102,7 @@ export default function Footer({sectionData}) {
                             {sectionData.contactData.global_facebook?.length > 0 && (
                                 <Link href={sectionData.contactData.global_facebook}
                                       target="_blank"
-                                      className="text-gray-700 hover:text-mainColorNormal transition-colors">
+                                      className="text-white\/50 hover:text-mainColorNormal transition-colors">
                                     <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             fillRule="evenodd"
@@ -115,7 +115,7 @@ export default function Footer({sectionData}) {
                             {sectionData.contactData.global_instagram?.length > 0 && (
                                 <Link href={sectionData.contactData.global_instagram}
                                       target="_blank"
-                                      className="text-gray-700 hover:text-mainColorNormal transition-colors">
+                                      className="text-white\/50 hover:text-mainColorNormal transition-colors">
                                     <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             fillRule="evenodd"
@@ -128,7 +128,7 @@ export default function Footer({sectionData}) {
                             {sectionData.contactData.global_twitter?.length > 0 && (
                                 <Link href={sectionData.contactData.global_twitter}
                                       target="_blank"
-                                      className="text-gray-700 hover:text-mainColorNormal transition-colors">
+                                      className="text-white\/50 hover:text-mainColorNormal transition-colors">
                                     <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
@@ -139,7 +139,7 @@ export default function Footer({sectionData}) {
                             {sectionData.contactData.global_linkedin?.length > 0 && (
                                 <Link href={sectionData.contactData.global_linkedin}
                                       target="_blank"
-                                      className="text-gray-700 hover:text-mainColorNormal transition-colors">
+                                      className="text-white\/50 hover:text-mainColorNormal transition-colors">
                                     <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
@@ -150,7 +150,7 @@ export default function Footer({sectionData}) {
                             {sectionData.contactData.global_youtube?.length > 0 && (
                                 <Link href={sectionData.contactData.global_youtube}
                                       target="_blank"
-                                      className="text-gray-700 hover:text-mainColorNormal transition-colors">
+                                      className="text-white\/50 hover:text-mainColorNormal transition-colors">
                                     <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"
@@ -161,7 +161,7 @@ export default function Footer({sectionData}) {
                             {sectionData.contactData.global_whatsapp?.length > 0 && (
                                 <Link href={sectionData.contactData.global_whatsapp}
                                       target="_blank"
-                                      className="text-gray-700 hover:text-mainColorNormal transition-colors">
+                                      className="text-white\/50 hover:text-mainColorNormal transition-colors">
                                     <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                                         <path
                                             d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.1-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"
