980bc143b5b263d00e3a4ddae448ccad3a88d561
dsz <1019217919@qq.com>
2026-02-25 23:09:15 +0800

    chore: footer样式

--- web/src/components/index/sections/template10/footer.jsx
+++ web/src/components/index/sections/template10/footer.jsx
@@ -59,7 +59,7 @@ export default function Footer({sectionData}) {
                                                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                                         </svg>
                                     </div>
-                                    <span className="text-white\/50">{sectionData.contactData.global_email}</span>
+                                    <span className="text-white\/50 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">{sectionData.contactData.global_email}</span>
                                 </li>
                                 <li className="flex items-start justify-center md:justify-start">
                                     <div className="text-mainColorNormal mr-2 mt-1">
@@ -69,7 +69,7 @@ export default function Footer({sectionData}) {
                                                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                                         </svg>
                                     </div>
-                                    <span className="text-white\/50">{sectionData.contactData.global_phone}</span>
+                                    <span className="text-white\/50 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">{sectionData.contactData.global_phone}</span>
                                 </li>
                                 <li className="flex items-start justify-center md:justify-start">
                                     <div className="text-mainColorNormal mr-2 mt-1">
@@ -81,7 +81,7 @@ export default function Footer({sectionData}) {
                                                   d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                                         </svg>
                                     </div>
-                                    <span className="text-white\/50">{sectionData.contactData.global_address}</span>
+                                    <span className="text-white\/50 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">{sectionData.contactData.global_address}</span>
                                 </li>
                             </ul>
                         </div>
@@ -91,9 +91,9 @@ export default function Footer({sectionData}) {
                     <div
                         className="flex flex-col sm:flex-row justify-between items-center border-t border-gray-400 pt-8">
                         <div className="flex flex-col text-sm text-gray-500 mb-4 sm:mb-0">
-                            <p>Copyright &copy; {new Date().getFullYear()} {sectionData.contactData.global_company_name}
+                            <p className="hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">Copyright &copy; {new Date().getFullYear()} {sectionData.contactData.global_company_name}
                             </p>
-                            <p className="mt-1">Powered by <a href="https://fktool.com" target="_blank"
+                            <p className="mt-1 hover:text-mainColorNormal transition-colors flex items-center justify-center md:justify-start">Powered by <a href="https://fktool.com" target="_blank"
                                                               rel="noopener noreferrer"
                                                               className="text-gray-500 hover:text-mainColorDeep">FK</a>
                             </p>
